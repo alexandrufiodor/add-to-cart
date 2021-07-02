0a2fbf15580035650c1df8a1e6b0b56165273dbc
Alexandru Fiodor <alexandrufiodor95@gmail.com>
2021-07-02 18:51:05 +0300

    Updates

---
+++ precache-manifest.22324f79df579e5d789a0aaf13128fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba476a5ca8dabdc89ced4427b017056",
+    "url": "/shop-test/index.html"
+  },
+  {
+    "revision": "d6ea6791e5735b87d676",
+    "url": "/shop-test/static/css/main.f1475735.chunk.css"
+  },
+  {
+    "revision": "29ac8a4278d1a9a3414f",
+    "url": "/shop-test/static/js/2.2d163959.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/shop-test/static/js/2.2d163959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ea6791e5735b87d676",
+    "url": "/shop-test/static/js/main.1a9f7466.chunk.js"
+  },
+  {
+    "revision": "c367ddd8e8f75acdee2a",
+    "url": "/shop-test/static/js/runtime-main.dd82bf0e.js"
+  }
+]);
